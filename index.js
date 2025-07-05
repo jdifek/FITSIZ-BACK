b8f58a2e842f8db059cd69189330af601d3e157e
jdifek <givotkov11@gmail.com>
2025-07-05 19:27:06 +0300

    wqwqwq

--- index.js
+++ index.js
@@ -25,11 +25,10 @@ const {
 
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.use(express.json());
-app.use(cors({
-  origin: '*',
-}));
+
+app.use(cors());
+
 
 // Публичные эндпоинты
 app.post('/api/register', register);
