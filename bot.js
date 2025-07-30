87955502171c5a6fd47578c630573f5c327dbf3c
jdifek <givotkov11@gmail.com>
2025-07-30 11:36:52 +0200

    wqwqwq

--- bot.js
+++ bot.js
@@ -6,28 +6,28 @@ const token = process.env.TELEGRAM_BOT_TOKEN;
 
 // Если ты на localhost — используй polling (без webhooks)
 const bot = new TelegramBot(token, { polling: true });
-
 bot.onText(/\/start/, async (msg) => {
-  const chatId = String(msg.user.id);
-  const firstName = msg.user.first_name || 'Без имени';
+  const userId = String(msg.from.id); // ← это ID пользователя (всегда)
+  const firstName = msg.from.first_name || 'Без имени';
 
   try {
     await prisma.user.upsert({
-      where: { telegramId: chatId },
+      where: { telegramId: userId },
       update: {
         isBotAvailable: true,
         firstName,
       },
       create: {
-        telegramId: chatId,
+        telegramId: userId,
         firstName,
         isBotAvailable: true,
       },
     });
 
-    await bot.sendMessage(chatId, '👋 Привет! Вы подписались на уведомления.');
-    console.log(`User ${chatId} подписался`);
+    await bot.sendMessage(msg.chat.id, '👋 Привет! Вы подписались на уведомления.');
+    console.log(`User ${userId} подписался`);
   } catch (err) {
     console.error('Ошибка при сохранении Telegram-пользователя:', err.message);
   }
 });
+
